643c7e3ee01c3591f3d0cbaac024d693a25bb2ac
Tim Evans <tevans@movableink.com>
2016-07-27 10:37:52 -0400

    fix BASE_URL

--- app/components/pl-uploader.js
+++ app/components/pl-uploader.js
@@ -2,5 +2,5 @@ import PlUploader from 'ember-plupload/components/pl-uploader';
 import config from '../config/environment';
 
 export default PlUploader.extend({
-  BASE_URL: config.PLUPLOAD_BASE_URL
+  BASE_URL: config.PLUPLOAD_BASE_URL || '/assets/'
 });
